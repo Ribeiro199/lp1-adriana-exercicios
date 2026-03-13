46c17e57a045c74185a20affa9ff5b276591a54b
Ribeiro199 <marcelo.celfroi@gmail.com>
2026-03-13 10:18:06 -0300

    Exercício 3 parte 2 lista de exercícios LP1.

---
+++ exer3b/Exer3b.java
@@ -0,0 +1,12 @@
+package exer3b;
+
+public class Exer3b {
+    public static void main(String[] args) {
+        double jan = 15000;
+        double fev = 23000;
+        double mar = 17000;
+        double sum = jan + fev + mar;
+        double med = sum / 3;
+        System.out.println("A soma é igual a: " + sum + "\nA média é igual a: " + med);
+    }
+}
